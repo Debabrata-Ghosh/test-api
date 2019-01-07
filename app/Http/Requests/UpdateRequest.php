db450c4793185f46848a31ce872bff8ae9977d28
Debu_Ghosh <debu@squaredworks.com>
2019-01-08 01:28:18 +0600

    change update

--- app/Http/Requests/UpdateRequest.php
+++ app/Http/Requests/UpdateRequest.php
@@ -24,10 +24,7 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         $rules=[];
-        $rules['first_name']='required|max:15';
-        $rules['last_name']='required|max:15';
-        $rules['country']='required|max:10';
-        $rules['phone']='required|max:20';
+        $rules['task']='required|max:50';
         return $rules;
     }
 }
