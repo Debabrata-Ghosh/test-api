013b1eba8c131046f2d1d5068099d0930ccb7d26
Debu_Ghosh <debu@squaredworks.com>
2019-01-08 01:06:40 +0600

    push

---
+++ app/Http/Repositories/ApiRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\User;
+use Hash;
+
+class ApiRepository
+{
+	public function insertOrUpdate($req,$id=null)
+	{
+		$user= new User();
+		if($req->id){
+			$user=$user->find($req->id);
+		}
+		$user->task=$req->task;
+		$success=$user->save();
+		return $success;
+	}
+
+	public function allData()
+	{
+		$data=User::all();
+		return $data;
+	}
+
+	public function oneData($id)
+	{
+		$data=User::where(['id'=>$id])->first();
+		return $data;
+	}
+
+	public function delete($id)
+	{
+		$success=User::where(['id'=>$id])->delete();
+		return $success;
+	}
+}
